3539051a3e5de449c9c27e17d82460c8ec095258
Anne-Okingo <annekadiso@gmail.com>
2025-07-29 11:30:07 +0300

    feat: improve the MPESA auth function with proper comments

--- netlify/functions/mpesa-auth.js
+++ netlify/functions/mpesa-auth.js
@@ -6,10 +6,12 @@ const headers = {
 };
 
 exports.handler = async (event, context) => {
+ // Handle Preflight (OPTIONS) Request
   if (event.httpMethod === 'OPTIONS') {
     return { statusCode: 200, headers, body: '' };
   }
 
+  //Reject Unsupported Methods
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
@@ -18,13 +20,16 @@ exports.handler = async (event, context) => {
     };
   }
 
+  //try-catch to handle unexpected errors without crashing
   try {
+    // Load Credentials and Environment URL,chooses between sandbox and production URLs based on the environment
     const consumerKey = process.env.MPESA_CONSUMER_KEY;
     const consumerSecret = process.env.MPESA_CONSUMER_SECRET;
     const baseUrl = process.env.NODE_ENV === 'production' 
       ? 'https://api.safaricom.co.ke' 
       : 'https://sandbox.safaricom.co.ke';
 
+    // Handle Missing Credentials
     if (!consumerKey || !consumerSecret) {
       return {
         statusCode: 400,
@@ -33,14 +38,18 @@ exports.handler = async (event, context) => {
       };
     }
 
+    //Generate Base64-encoded Auth Token
     const auth = Buffer.from(`${consumerKey}:${consumerSecret}`).toString('base64');
     
+   // Sends a GET request to request an access token adding the encoded auth as a header.
     const response = await fetch(`${baseUrl}/oauth/v1/generate?grant_type=client_credentials`, {
       method: 'GET',
       headers: {
         'Authorization': `Basic ${auth}`,
       }
     });
+
+    // Parses the JSON response to access access_token, expires_in, etc.
     const data = await response.json();
     
         if (!response.ok){
@@ -48,6 +57,7 @@ exports.handler = async (event, context) => {
           throw new Error('Failed to get M-Pesa access token');
         }
     
+    //If token was returned, return it to the frontend/client.
     if (data.access_token) {
       return {
         statusCode: 200,
