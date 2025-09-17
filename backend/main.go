469241a9ffb91a2f7f2b1abbe381ac779236aeee
andyosyndoh <andyovvo8@gmail.com>
2025-09-17 07:42:31 +0300

    fix: restrict CORS origins to specific domains for enhanced security

--- backend/main.go
+++ backend/main.go
@@ -44,7 +44,7 @@ func main() {
 
 	// CORS middleware
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
+		AllowOrigins:     []string{"https://deploy-preview-17--healmentalcare.netlify.app/","https://healmentalcare.netlify.app/"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
