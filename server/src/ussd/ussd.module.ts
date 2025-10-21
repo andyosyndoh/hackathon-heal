38a1c4565792ebf34b4d8f2b7ec8d0eee718f412
Anne-Okingo <annekadiso@gmail.com>
2025-10-21 16:56:38 +0300

    feat: import the chatmodele into the Ussd modele

--- server/src/ussd/ussd.module.ts
+++ server/src/ussd/ussd.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UssdController } from './ussd.controller';
 import { UssdService } from './ussd.service';
+import { ChatModule } from '../chat/chat.module';
 
 @Module({
-  imports: [],
+  imports: [ChatModule],
   controllers: [UssdController],
   providers: [UssdService],
   exports: [UssdService],
