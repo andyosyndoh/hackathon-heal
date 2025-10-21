569fe01c47edc6ed0963dce81887cb4ae62a4f70
Anne-Okingo <annekadiso@gmail.com>
2025-10-21 17:59:07 +0300

    feat: add ConfigModule to load environment variables

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
@@ -13,6 +14,11 @@ import { UssdModule } from './ussd/ussd.module';
 
 @Module({
   imports: [
+    // Load environment variables from .env file
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'heal.db',
