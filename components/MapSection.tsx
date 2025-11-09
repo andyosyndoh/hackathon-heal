96fac01fb44e94c5f5816879fbe09fe0fe2537a6
adaken4 <adakennedy6@gmail.com>
2025-11-09 13:54:41 +0300

    feat: add MapSection component with Leaflet map integration and custom marker icons

---
+++ components/MapSection.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import React from 'react';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
+// Fix for default marker icon not showing up
+// This is a common issue with react-leaflet and webpack
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon-2x.png',
+  iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+  shadowUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-shadow.png',
+});
+
+const Map = () => {
+  const position = [51.505, -0.09]; // Placeholder for your office coordinates
+
+  return (
+    <div className="w-full" style={{ height: '500px' }}>
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="w-full h-full rounded-lg shadow-lg">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}></Marker>
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
